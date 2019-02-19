a1b7f71bdbc4189c2fc2cbba749f75824f4a5fc9
ncristiansa <ncristiansalinasandia@gmail.com>
2019-02-19 19:22:16 +0100

    Vista cliente estilo

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -7,8 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
-<p>Funciona</p>
+<body style="margin:2%; background-color: #f3f0f0;">
+<div class="row">
+    <div class="col-12" align="center" style="margin-bottom:2%;">
+        <button type="button" class="btn btn-success">Agregar Cliente</button>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12" style="background-color:white;padding:2%; border:1px solid; box-shadow:1px 1px 1px 1px #888888;">
+        <p>Listado de Clientes</p>
+    </div>
+</div>
+
     
 </body>
 </html>
