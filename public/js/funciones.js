321fc876580a305592857b065845543b16ddea10
ncristiansa <ncristiansalinasandia@gmail.com>
2019-02-23 18:47:06 +0100

    Prueba consulta

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -22,5 +22,47 @@ function moduloError(error) {
 
 }
 
-$(document).ready();
+function generaTabla(Consulta){
+	console.log("Entra en la funcion.");
+	if(Consulta instanceof Array){
+		
+		var h2 = $('h2');
+		var divTabla = $('<div>').addClass("table-responsive");
+		var tabla = $('<table>').addClass("table table-hover");
+		var thead = $('<thead>').addClass("thead-dark");
+		var tbody = $('<tbody>');
+		var tr_titulos = $('<tr>');
+		var tr_valores = $('<tr>')
+		for(var datos in Consulta){
+			var Claves = Object.keys(Consulta[datos]);
+			var Valores = Object.values(Consulta[datos]);
+			for(var key in Claves){
+				console.log(Claves[key]);
+				var thTitulo = $('<th>').attr('scope', 'col');
+				thTitulo.text(Claves[key]);
+				tr_titulos.append(thTitulo);
+
+			}
+			for(var value in Valores){
+				var thValor = $('<th>');
+				var tdValor = $('<td>');
+				tdValor.text(Valores[value]);
+				console.log(Valores[value]);
+				tr_valores.append(tdValor);
+			}
+		}
+		
+		tbody.append(tr_valores);
+		thead.append(tr_titulos);
+		tabla.append(thead);
+		tabla.append(tbody);
+		
+		divTabla.append(tabla);
+		h2.after(divTabla);
+	}else{
+		console.log("El parametro que has introducido no es un Array, por favor comprueba que lo sea.")
+	}
+
+}
+
 
--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -5,37 +5,20 @@
     </div>
 </div>
 @section('content')
+<?php
+    $datos_cliente = DB::table('clientes')->select('id','Nombre', 'Email', 'Telefono','Provincia', 'NIF/CIF')->get();
+?>
 <div class="row">
     <div class="col-12">
         <h2>Listado de Clientes</h2>
         <br>
-        <div class="table-responsive">
-            <table class="table table-hover" style="border:1px solid;">
-                <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Cod. Cliente</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Provincia</th>
-                </tr>
-                </thead>
-                    <tbody>
-                    <?php
-                    $datos_cliente = DB::table('clientes')->get();
-                    foreach ($datos_cliente as $key => $value) {
-                        echo"<tr>";
-                            echo "<th scope='row'>".$value->id."</th>";
-                            echo "<th scope='row'>".$value->Nombre."</th>";
-                            echo "<th scope='row'>".$value->Email."</th>";
-                            echo "<th scope='row'>".$value->Provincia."</th>";
-                        echo"</tr>";
-                    }
-                    ?>
-                    </tbody>
-            </table>
-        </div>
         <br>
         <button type="button" class="btn btn-success">Agregar Cliente</button>
     </div>
 </div>
+    <script type="text/javascript">
+        
+        var Consulta = <?php echo json_encode($datos_cliente);?>;
+        generaTabla(Consulta);       
+    </script>
 @stop
